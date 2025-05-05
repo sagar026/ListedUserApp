1cc3f3b047dfe977df5f4f83e59085f5b9b2f812
mdSameer <mdsameeransari001@gmail.com>
2025-05-05 17:07:15 +0530

    Initial Commit

---
+++ ListedUserApp/ListedUserAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListedUserAppApp.swift
+//  ListedUserApp
+//
+//  Created by Vidhyasagar on 05/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ListedUserAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
